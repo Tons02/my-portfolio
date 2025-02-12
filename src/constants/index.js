7192de4f80968f8e4780804427d3c30aa61d3a17
Antonio Jr. D. Montilla <system.montilla@gmail.com>
2025-02-12 09:39:25 +0800

    update my information

--- src/constants/index.js
+++ src/constants/index.js
@@ -38,75 +38,190 @@ import Booking from "../assets/projects/Booking.png";
 import BookingSecondImage from "../assets/projects/Booking-1.png";
 import BookingThirdImage from "../assets/projects/Booking-2.png";
 import BookingFourthImage from "../assets/projects/Booking-3.png";
-
+import Locus from "../assets/projects/Locus.png";
 
 export const HERO_CONTENT = `
-I am a passionate backend developer with two years of experience in crafting robust and scalable web applications using technologies like Laravel API (Laravel 10, Eloquent, and the Laravel API Toolkit), MySQL, Node.js, Express, and MongoDB, and I also build applications with WordPress, Elementor, Elementor Pro, WooCommerce, and WCFM multi-vendor, aiming to leverage my skills to design innovative solutions that drive business growth and enhance user experiences.`;
+I am a passionate junior backend developer with two years of experience in crafting robust and scalable web applications using technologies like Laravel API (Laravel 10, Eloquent, and the Laravel API Toolkit), MySQL, Node.js, Express, and MongoDB, and I also build applications with WordPress, Elementor, Elementor Pro, WooCommerce, and WCFM multi-vendor, aiming to leverage my skills to design innovative solutions that drive business growth and enhance user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated backend developer with a Bachelor of Science in Information Technology and practical experience as an Entry-Level Developer at RDF Feed, Livestock & Foods, Inc. Throughout my journey, I have honed my skills in delivering effective solutions and managing relational databases using MySQL and Laravel API. This experience has equipped me with a solid foundation in backend development, including proficiency in Laravel, MySQL, and API integration, preparing me to contribute effectively to innovative projects that drive business success.`;
+export const ABOUT_TEXT = `I am a dedicated junior backend developer with a Bachelor of Science in Information Technology and practical experience as an Entry-Level Developer at RDF Feed, Livestock & Foods, Inc. Throughout my journey, I have honed my skills in delivering effective solutions and managing relational databases using MySQL and Laravel API. This experience has equipped me with a solid foundation in backend development, including proficiency in Laravel, MySQL, and API integration, preparing me to contribute effectively to innovative projects that drive business success.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
+    year: "February 2025 - Present",
+    role: "Junior Developer",
+    company: "RDF Feed, Livestock & Foods, Inc",
+    description:
+      "After demonstrating my skills and contributions as an Entry-Level Developer, I was promoted to Junior Developer at RDF Feed, Livestock & Foods, Inc. In this role, I take on more responsibilities in backend development, API integrations, and system optimization. I work with Laravel 11, Node.js, and Express to develop scalable applications, leveraging MongoDB for efficient data management. My expertise extends to WordPress, WooCommerce, and WCFM multi-vendor, where I enhance e-commerce functionalities. I continue to refine my skills in API development and testing with Postman and Laravel API Toolkit, ensuring seamless performance and user experience.",
+    technologies: [
+      "Laravel 11",
+      "Node JS",
+      "Express",
+      "MongoDB",
+      "Postman",
+      "Laravel API Toolkit",
+      "WordPress",
+      "WooCommerce",
+      "WCFM - Multi-Vendor",
+      "Elementor",
+      "Elementor Pro",
+    ],
+  },
+  {
+    year: "October 2023 - January 2025",
     role: "Entry-Level Developer",
     company: "RDF Feed, Livestock & Foods, Inc",
-    description: "In my role as an Entry-Level Developer at RDF Feed, Livestock & Foods, Inc, I contribute to the development of innovative web applications and platforms. Utilizing Laravel 10 for backend development, I create and maintain robust APIs to support various functionalities. My experience with Node.js and Express allows me to build scalable applications, while MongoDB serves as my database solution for efficient data management. I leverage Postman for API testing and utilize the Laravel API Toolkit for streamlined development processes. Additionally, I work with WordPress and WooCommerce, enhancing e-commerce capabilities through WCFM multi-vendor functionality, and design visually appealing and user-friendly interfaces using Elementor and Elementor Pro. I am dedicated to continuous learning and actively collaborating with team members to ensure project success.",
-    technologies: ["Laravel 10", "Node JS", "Express", "mongoDB", "Postman", "Laravel API tool kit", "Wordpress", "Woocommerce", "WCFM - multi-vendor", "Elementor", "Elementor Pro"],
+    description:
+      "In my role as an Entry-Level Developer at RDF Feed, Livestock & Foods, Inc, I contribute to the development of innovative web applications and platforms. Utilizing Laravel 10 for backend development, I create and maintain robust APIs to support various functionalities. My experience with Node.js and Express allows me to build scalable applications, while MongoDB serves as my database solution for efficient data management. I leverage Postman for API testing and utilize the Laravel API Toolkit for streamlined development processes. Additionally, I work with WordPress and WooCommerce, enhancing e-commerce capabilities through WCFM multi-vendor functionality, and design visually appealing and user-friendly interfaces using Elementor and Elementor Pro. I am dedicated to continuous learning and actively collaborating with team members to ensure project success.",
+    technologies: [
+      "Laravel 10",
+      "Node JS",
+      "Express",
+      "mongoDB",
+      "Postman",
+      "Laravel API tool kit",
+      "Wordpress",
+      "Woocommerce",
+      "WCFM - multi-vendor",
+      "Elementor",
+      "Elementor Pro",
+    ],
   },
   {
     year: "2022 - Present",
     role: "Freelancer Developer",
     company: "",
-    description: "As a freelance developer, I specialize in creating robust web applications using PHP and Laravel 10. My work involves collaborating with various clients to understand their needs and deliver tailored solutions. I focus on backend development, utilizing mySQL for database management and ensuring data integrity. Additionally, I use Postman for API testing, and I am proficient in front-end technologies including JavaScript, HTML, and CSS, allowing me to create seamless user experiences. My role encompasses the entire development lifecycle, from initial design to deployment and ongoing support, ensuring that client projects are completed on time and to specification.",
-    technologies: ["PHP", "Laravel 10", "mySQL", "Postman", "Javascript", "HTML", "CSS"],
+    description:
+      "As a freelance developer, I specialize in creating robust web applications using PHP and Laravel 10. My work involves collaborating with various clients to understand their needs and deliver tailored solutions. I focus on backend development, utilizing mySQL for database management and ensuring data integrity. Additionally, I use Postman for API testing, and I am proficient in front-end technologies including JavaScript, HTML, and CSS, allowing me to create seamless user experiences. My role encompasses the entire development lifecycle, from initial design to deployment and ongoing support, ensuring that client projects are completed on time and to specification.",
+    technologies: [
+      "PHP",
+      "Laravel 10",
+      "mySQL",
+      "Postman",
+      "Javascript",
+      "HTML",
+      "CSS",
+    ],
   },
 ];
 
 export const PROJECTS = [
   {
+    title: "Locus",
+    year: "January 2025 - Ongoing development",
+    image: [Locus],
+    link: "#",
+    description:
+      "Locus is a system designed to support RDF’s research team in identifying optimal locations for new store establishments. It streamlines data collection, analysis, and decision-making by prioritizing research-driven insights before finalizing store locations.",
+    technologies: [
+      "Laravel 11",
+      "Reverb",
+      "REST API",
+      "PHP",
+      "mySQL",
+      "Laravel API Toolkit",
+      "Postman",
+    ],
+  },
+  {
     title: "RDF Main Canteen",
     year: "May 2024 - Ongoing development",
-    image: [RdfCanteen, RdfCanteenSecondImage, RdfCanteenThirdImage, RdfCanteenFourthImage, RdfCanteenFifthImage],
+    image: [
+      RdfCanteen,
+      RdfCanteenSecondImage,
+      RdfCanteenThirdImage,
+      RdfCanteenFourthImage,
+      RdfCanteenFifthImage,
+    ],
     link: "#",
     description:
       "This project is designed to streamline the canteen ordering process for RDF Feed, Livestock & Foods, Inc employees. It allows employees to place orders directly from the canteen and track their purchase history. The system also manages employee orders efficiently, ensuring smooth operations and real-time monitoring of purchases. Built with WooCommerce and WCFM multi-vendor, it integrates seamlessly with the WordPress environment to offer a complete e-commerce solution.",
-    technologies: ["HTML", "CSS", "WordPress", "PHP", "mySQL", "WooCommerce", "WCFM multi-vendor", "Elementor", "Elementor Pro"],
-  },  
+    technologies: [
+      "HTML",
+      "CSS",
+      "WordPress",
+      "PHP",
+      "mySQL",
+      "WooCommerce",
+      "WCFM multi-vendor",
+      "Elementor",
+      "Elementor Pro",
+    ],
+  },
   {
     title: "Eats Quick!",
-    year: "May 2024 - Ongoing development",
-    image: [EatsQuick, EatsQuickSecondImage, EatsQuickThirdImage, EatsQuickFourthImage, EatsQuickFifthImage],
+    year: "May 2024 - December 2024",
+    image: [
+      EatsQuick,
+      EatsQuickSecondImage,
+      EatsQuickThirdImage,
+      EatsQuickFourthImage,
+      EatsQuickFifthImage,
+    ],
     link: "https://eatsquick.ph/",
     description:
       "Eats Quick! is an online ordering system developed for a business under RDF Feed, Livestock & Foods, Inc. It allows customers to conveniently place orders online and pick them up at the store. The system simplifies the ordering process by offering an easy-to-use interface for browsing the menu, selecting items, and scheduling pickup. It is designed to streamline operations and enhance the customer experience with fast and efficient service.",
-      technologies: ["HTML", "CSS", "WordPress", "PHP", "mySQL", "WooCommerce", "WCFM multi-vendor", "Elementor", "Elementor Pro", "Paymongo"],
-  },  
+    technologies: [
+      "HTML",
+      "CSS",
+      "WordPress",
+      "PHP",
+      "mySQL",
+      "WooCommerce",
+      "WCFM multi-vendor",
+      "Elementor",
+      "Elementor Pro",
+      "Paymongo",
+    ],
+  },
   {
     title: "Customer Satisfaction Survey",
-    year: "February 2024 - Ongoing development",
-    image: [CustomerSatisfactionSurvey, CustomerSatisfactionSurveySecondImage, CustomerSatisfactionSurveyFifthImage, CustomerSatisfactionSurveyThirdImage, CustomerSatisfactionSurveyFourthImage, CustomerSatisfactionSurveySixImage, CustomerSatisfactionSurveySevenImage],
+    year: "February 2024 - November 2024",
+    image: [
+      CustomerSatisfactionSurvey,
+      CustomerSatisfactionSurveySecondImage,
+      CustomerSatisfactionSurveyFifthImage,
+      CustomerSatisfactionSurveyThirdImage,
+      CustomerSatisfactionSurveyFourthImage,
+      CustomerSatisfactionSurveySixImage,
+      CustomerSatisfactionSurveySevenImage,
+    ],
     link: "https://customersurvey.freshoption.ph/",
     description:
       "This system was developed for Fresh Option to reward customers who complete surveys. Based on their responses to pre-defined questions, customers receive a voucher code that can be redeemed at any Fresh Option branch. My focus was on the backend, utilizing Laravel 10 and Eloquent to create a seamless survey and rewards process.",
-    technologies: ["PHP", "Laravel 10", "Eloquent", "MySQL", "Laravel API", "Laravel API Toolkit", "Postman"],
+    technologies: [
+      "PHP",
+      "Laravel 10",
+      "Eloquent",
+      "MySQL",
+      "Laravel API",
+      "Laravel API Toolkit",
+      "Postman",
+    ],
   },
   {
     title: "Metro GPS",
     year: "November 2023 - Ongoing development",
-    image: [MetroGpsSecondImage, MetroGPS,  MetroGpsThirdImage],
+    image: [MetroGpsSecondImage, MetroGPS, MetroGpsThirdImage],
     link: "https://metrogps.rdfmis.com/",
-    description:"Metro GPS is a tracking system used by RDF Feed, Livestock & Foods, Inc drivers to monitor their routes and generate reports. The system enables real-time tracking of drivers and allows for the extraction of detailed reports on their activities. I worked on the backend of this project, focusing on building APIs using Node.js and MongoDB. Additionally, the system integrates Express and Multer for efficient file handling and report generation.",
+    description:
+      "Metro GPS is a tracking system used by RDF Feed, Livestock & Foods, Inc drivers to monitor their routes and generate reports. The system enables real-time tracking of drivers and allows for the extraction of detailed reports on their activities. I worked on the backend of this project, focusing on building APIs using Node.js and MongoDB. Additionally, the system integrates Express and Multer for efficient file handling and report generation.",
     technologies: ["Node JS", "Express", "Multer", "MongoDB", "Postman"],
-  },  
+  },
   {
     title: "Loan Management System",
     year: "November 2023 - January 2024",
-    image: [LoanManagementSystem, LoanManagementSystemSecondImage, LoanManagementSystemThirdImage, LoanManagementSystemFourthImage, LoanManagementSystemFifthImage],
+    image: [
+      LoanManagementSystem,
+      LoanManagementSystemSecondImage,
+      LoanManagementSystemThirdImage,
+      LoanManagementSystemFourthImage,
+      LoanManagementSystemFifthImage,
+    ],
     link: "https://loan-management-system.great-site.net/",
     description:
       "This system is designed to monitor clients' loan balances and transactions. Users can apply for loans, approve loans for others, and receive notifications for pending payments. It also allows users to upload images as proof of payment, providing a streamlined and efficient loan management process.",
     technologies: ["HTML", "CSS", "PHP", "JavaScript", "mySQL"],
-  },  
+  },
   {
     title: "I-Work",
     year: "May 2023 - November 2023",
@@ -124,20 +239,28 @@ export const PROJECTS = [
     description:
       "This system, developed as a freelance full-stack native PHP project for a private company, provides an efficient online booking management solution for the resort. It streamlines the reservation process by allowing customers to view availability, make bookings, and manage their reservations seamlessly. The system automates confirmations and reminders, reducing manual workload for staff and minimizing booking errors. By centralizing booking information, it enhances customer experience and helps the resort optimize occupancy rates and manage their resources more effectively.",
     technologies: ["HTML", "CSS", "PHP", "JavaScript", "mySQL"],
-  }, 
+  },
   {
     title: "E-DUKASYON",
     year: "May 2022 - November 2022",
-    image: [EDukasyon, EDukasyonSecondImage, EDukasyonThirdImage, EDukasyonFourthImage, EDukasyonFifthImage, EDukasyonSixImage, EDukasyonSevenImage],
+    image: [
+      EDukasyon,
+      EDukasyonSecondImage,
+      EDukasyonThirdImage,
+      EDukasyonFourthImage,
+      EDukasyonFifthImage,
+      EDukasyonSixImage,
+      EDukasyonSevenImage,
+    ],
     link: "#",
     description:
       "This project was developed as part of my college capstone thesis. It aims to assist students by providing an efficient way to take quizzes, monitor their grades, and upload modules and assignments. The system was designed to streamline academic tasks for both students and educators.",
     technologies: ["HTML", "CSS", "PHP", "JavaScript", "mySQL"],
-  }  
+  },
 ];
 
 export const CONTACT = {
-  address: "Pampanga Philippines",
+  address: "Philippines",
   // phoneNo: "+12 4555 666 00 ",
   // email: "tonsmontilla@gmail.com",
 };
